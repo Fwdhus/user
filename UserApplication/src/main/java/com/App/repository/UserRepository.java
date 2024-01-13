a658fedc2db84ed255df20814d9a6583cae17fd8
Fwdhus <74827573+Fwdhus@users.noreply.github.com>
2024-01-13 22:10:18 +0000

    Delete UserApplication directory

--- UserApplication/src/main/java/com/App/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.App.repository;
-
-import com.App.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-}
--- UserApplication/src/main/java/com/App/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.App.service;
-
-import com.App.domain.User;
-
-import java.util.List;
-
-public interface UserService {
-    List<User> getAllUsers();
-}
