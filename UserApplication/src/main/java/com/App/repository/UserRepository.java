a2916789fae8b14f09930d30491c4712996da044
Fwdhus <74827573+Fwdhus@users.noreply.github.com>
2024-01-13 22:03:48 +0000

    Add files via upload

---
+++ UserApplication/src/main/java/com/App/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.App.repository;
+
+import com.App.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
---
+++ UserApplication/src/main/java/com/App/service/UserService.java
@@ -0,0 +1,9 @@
+package com.App.service;
+
+import com.App.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+}
